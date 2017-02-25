0860c5f2888962edc3a14bdd5da84496addea75d
Richard Covington <rcovington79@gmail.com>
2017-02-25 13:03:50 -0500

    Add files via upload

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,12 +19,12 @@
 
 #define BLUETOOTH_QTI_SW TRUE
 
-#define BTM_DEF_LOCAL_NAME "LG G3 D850"
+#define BTM_DEF_LOCAL_NAME "LG G3 (D855)"
 
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS 16
-#define BLE_VND_INCLUDED TRUE
+ #define BLE_VND_INCLUDED TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD FALSE
 #define BLE_PERIPHERAL_ADV_NAME FALSE
--- full_d850.mk
+++ full_d850.mk
@@ -17,7 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from d850 device
+# Inherit from d855 device
 $(call inherit-product, device/lge/d850/device.mk)
 
 # Set those variables here to overwrite the inherited values.
